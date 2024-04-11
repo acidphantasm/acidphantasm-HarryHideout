c2ebec81160e3bab1ae7bba08e55a84c9a299e7e
acidphantasm <justincote@gmail.com>
2024-04-11 00:24:59 -0500

    cleanup

--- types/utils/HttpFileUtil.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="node" />
-import { ServerResponse } from "node:http";
-import { HttpServerHelper } from "@spt-aki/helpers/HttpServerHelper";
-export declare class HttpFileUtil {
-    protected httpServerHelper: HttpServerHelper;
-    constructor(httpServerHelper: HttpServerHelper);
-    sendFile(resp: ServerResponse, filePath: string): void;
-}
